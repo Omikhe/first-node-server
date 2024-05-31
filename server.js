2b5cc8d74da3cd25a32b59f68d733b1fef4818e6
Omikhe <msakamike84@gmail.com>
2024-05-31 22:36:53 +0200

    add the listner thingy!

--- server.js
+++ server.js
@@ -16,3 +16,11 @@ const server = http.createServer((req, res) => {
     })
 
 })
+
+server.listen(port, (error) => {
+    if (error) {
+        console.log("Something went wrong", error)
+    } else {
+        console.log("Server is listening on port " + port);
+    }
+})
